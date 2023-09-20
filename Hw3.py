89f4c6ed835f836f2a17712c2bf7f95c08fc0366
yevheniiakolesnyk <evgeniakolesnik11@gmail.com>
2023-09-20 19:01:09 +0300

    Hw3.py

--- Hw3.py
+++ Hw3.py
@@ -1,27 +1,31 @@
 # Home task_1
 test_word = str(input('Enter any word:'))
-remove_letter = str(input('Specify some word\'s letter to be removed:'))
+remove_letter = input('Specify some word\'s letter to be removed:')
 corrected_sentence = (test_word.replace(remove_letter,''))
 print(f'Check what is done: {corrected_sentence}')
+
 #Home task_2
-string_initial_state = str(input('Type some sentence:'))
+string_initial_state = input('Type some sentence:')
 string_medium_state = string_initial_state.lower()
 string_ready_state = string_medium_state.title()
 print(f'Get the correct typed string: {string_ready_state}')
+
 #Home task_3
-initial_string = str(input('Type some sentence and wait for the magic:'))
-finished_string = (initial_string[::-1])
+initial_string = input('Type some sentence and wait for the magic:')
+finished_string = initial_string[::-1]
 print(f'Magic is done: {finished_string}')
+
 #Home task_4
-string_1 = str(input('Enter any sentence you want:'))
-string_2 = str(input('Enter one more sentence:'))
-is_the_same = (string_1 == string_2)
-print(f'These values are identical: {is_the_same}')
+string_1 = input('Enter any sentence you want:')
+string_2 = input('Enter one more sentence:')
+print(f'These values are identical: {string_1 == string_2}')
+
 #Home task_5
-repeat_string = str((input('Enter some letters:')))
+repeat_string = input('Enter some letters:')
 numbers_amount = int(input('Enter how many times the letters should be repeated:'))
 result = repeat_string * numbers_amount
 print(f'Check that the letters are repeated {numbers_amount} times:{result}')
+
 #Home task_6
 uah_1 = float(input('Enter how many uah you want to exchange:'))
 dollar_rate1 = float(input('Enter today\'s dollar rate:'))
