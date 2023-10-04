d2832a0b7660c4b5cae9e17e222ba9a9b4d9b01f
Yevheniia Kolesnyk <145061254+yevheniiakolesnyk@users.noreply.github.com>
2023-10-04 22:46:07 +0300

    Update Task3 corrected

--- Hw6/Task3.py
+++ Hw6/Task3.py
@@ -1,12 +1,14 @@
 playlist = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
-play_number = int(input('Enter how many how many petals you want to extract (up to 6 inclusive) :'))
-
+play_number = int(input('Enter how many how many petals you want to extract:'))
 
 if play_number in range(1, 7):
     ans = playlist[play_number - 1]
     print(f'a prediction for the day: {ans}')
 elif play_number >= 7:
-    print(f'The number {play_number} is not valid.')
+    general_amount = len(playlist)
+    ind = (play_number - 1) % general_amount
+    ans_1 = playlist[ind]
+    print(f'a prediction for the day: {ans_1}')
 elif play_number <= 0:
     print(f'The number {play_number} is not valid.')
-
+    
