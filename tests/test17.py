73fe4ececfc5d47e839a9cc08eed52680ebccaeb
yevheniiakolesnyk <evgeniakolesnik11@gmail.com>
2023-11-13 23:12:29 +0200

    Hw17

---
+++ tests/test17.py
@@ -0,0 +1,18 @@
+from src import my_func17
+
+
+def test_add():
+    assert my_func17.add(2, 3)
+
+def test_minus():
+    assert my_func17.minus(4, 1)
+
+def test_multiple():
+    result = my_func17.multiplying(3, 12)
+    assert isinstance(result, int), f"Expected an integer, but got {type(result)}"
+
+def test_division():
+    assert my_func17.division(6, 2)
+
+def test_square():
+    assert my_func17.square(4)
